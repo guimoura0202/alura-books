f96fb6af7f73a0367960a9f0634f1ca6ae51e129
Guilherme de Ávila de Moura <guiavilamou@gmail.com>
2024-02-04 23:38:54 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
+        <ul>
+          <li><p>CATEGORIAS</p></li>
+          <li><p>MINHA ESTANTE</p></li>
+          <li><p>FAVORITOS</p></li>
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,8 +1,9 @@
 import logo from '../../imagens/logo.svg';
+import './style.css';
 function Logo() {
     return (
         <div className='logo'>
-          <img src={logo} alt='Logo da Alura Books'></img>
+          <img src={logo} alt='Logo da Alura Books' className='image-logo'></img>
           <p><strong>Alura </strong>Books</p>
         </div>
     )
