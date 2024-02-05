dbf2ca85b9963f6c3eb88f71f200f034c5cc5133
Guilherme de Ávila de Moura <guiavilamou@gmail.com>
2024-02-05 12:22:21 -0300

    header

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,14 @@
+import Logo from '../Logo';
+import OpcoesHeader from '../OpcoesHeader';
+import IconesHeader from '../IconesHeader';
+import './style.css'
+function Header (){
+    return(
+        <header className='App-header'>
+            <Logo/>
+            <OpcoesHeader/>
+            <IconesHeader/>
+        </header>
+    )
+}
+export default Header;
